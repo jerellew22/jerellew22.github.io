9716ae73dd2702a56125632baffd8f474dff041f
Jerel Lewis <Jerellew22@yahoo.com>
2016-02-28 22:08:14 -0600

    homeworknthings

---
+++ week_7_homework_javscrpt.js
@@ -0,0 +1,18 @@
+var sport = prompt ("what is your favorite sport");
+
+switch (sport){
+    case 'football':
+      console.log ("the greatest sport, the sport of kings");
+      break;
+    case 'basketball':
+      console.log ("Another great sport, not the greatest though");
+      break;
+    case 'baseball':
+        console.log ("it's an ok sport, just not on the level of football or even basketball");
+        break;
+    case 'soccer':
+        console.log ("Ummm, we live in the USA, good choice in Europe perhaps");
+        break;
+    default:
+         console.log ("are you even serious??")
+}
